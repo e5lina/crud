865cb56ef4f0a0f6fac9ee94c4b37ec7bc901718
Gojeonghun <apple@appleui-MacBookPro.local>
2022-11-13 14:35:14 +0900

    수정

---
+++ Desktop/crud/src/pages/Main.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Box = styled.div``;
+
+const MainBox = styled.div``;
+function Main() {
+  return (
+    <Box>
+      <MainBox>fekjfe</MainBox>
+    </Box>
+  );
+}
+
+export default Main;
